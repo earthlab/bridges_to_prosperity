c7ac3c57076f9494d23b67f12374e6b3fc5dd258
Ckster <erve3705@colorado.edu>
2022-11-02 10:36:16 -0600

    Getting rid of shape files

--- tiling_and_inference/b2p_tiling.py
+++ tiling_and_inference/b2p_tiling.py
@@ -11,14 +11,27 @@ import geopandas as gpd
 import rasterio
 from fastai.vision import *
 from osgeo import gdal
-import rioxarray
 from shapely.geometry import Polygon
+import geojson
+from geojson import Polygon, Feature, FeatureCollection
+from contextlib import redirect_stdout, redirect_stderr, ExitStack
 
 BIN_DIR = os.path.dirname(__file__)
 
 
 def scale(x) -> float:
     return (x - np.nanmin(x)) * (1 / (np.nanmax(x) - np.nanmin(x)) * 255)
+
+
+@contextmanager
+def suppress(out=True, err=False):
+    with ExitStack() as stack:
+        with open(os.devnull, "w") as null:
+            if out:
+                stack.enter_context(redirect_stdout(null))
+            if err:
+                stack.enter_context(redirect_stderr(null))
+            yield
 
 
 def progress_tiff_list(filename: str) -> List[str]:
@@ -148,7 +161,7 @@ def write_tiff_files(input_rstr: str, name: str, tiling_dir: str, output_dir: st
     if bucket_name is not None and s3 is not None:
         s3.Bucket(bucket_name).upload_file(output_scaled, name + '_' + output_scaled)
 
-    del r, g, b, dss, true
+    del r, g, b, true
 
     dem = gdal.Open(output_scaled)
     gt = dem.GetGeoTransform()
@@ -165,93 +178,58 @@ def write_tiff_files(input_rstr: str, name: str, tiling_dir: str, output_dir: st
     ysteps = [ymax - ysize * i for i in range(div + 1)]
 
     del gt, xmin, ymax, res, xlen, ylen
-
+    geoms = {}
+    features = []
+    filenames = []
     for i in range(div):
         for j in range(div):
             xmin = xsteps[i]
             xmax = xsteps[i + 1]
             ymax = ysteps[j]
             ymin = ysteps[j + 1]
-            gdal.Warp(os.path.join(tiling_dir, 'dem' + input_rstr.split('.')[0][-3:] + str(i) + str(j)
-                                   + '.tif'), dem, outputBounds=(xmin, ymin, xmax, ymax), dstNodata=-999)
+            tiff_filename = os.path.join(tiling_dir, 'dem' + input_rstr.split('.')[0][-3:] + str(i) + str(j) + '.tif')
+            filenames.append(tiff_filename)
+            gdal.Warp(tiff_filename, dem, outputBounds=(xmin, ymin, xmax, ymax), dstNodata=-999)
+            coords = ((xmin, ymin), (xmax, ymin), (xmax, ymax), (xmin, ymax))
+            geoms[tiff_filename] = {'geometry': Polygon(coords)}
+            features.append(
+                {
+                    "type": "Feature",
+                    "properties": {},
+                    "geometry": {
+                        "type": "Polygon",
+                        "coordinates": [
+                            [[xmin, ymin], [xmax, ymin], [xmax, ymax], [xmin, ymax], [xmin, ymin]]
+                        ]
+                    }
+                }
+            )
             del xmin, xmax, ymax, ymin
+
+    print(f'tiff {len(filenames)}, {len(set(filenames))}')
+
+    with open(os.path.join(output_dir, 'tiff_geoms.geojson'), 'w+') as f:
+        geojson.dump({'geoms': geoms}, f)
+
+    with open(os.path.join(output_dir, 'plotting_geoms.geojson'), 'w+') as f:
+        crs = {
+            "type": "name",
+            "properties": {
+                "name": str(dss.crs)
+            }
+        }
+        geojson.dump(FeatureCollection(features, crs=crs), f)
+
     del dem, div, xsteps, ysteps
     logging.info(f'Wrote tiff files in {time.time() - t1}s')
     print(f'Wrote tiff files to {tiling_dir}')
 
-
-def write_shape_files(input_rstr: str, name: str, tiling_dir: str, shape_dir: str, output_dir: str, cores: int,
-                      bucket_name: str = None, s3=None) -> str:
-    # Get the coordinate reference system of the tile
-    rds = rioxarray.open_rasterio(os.path.join(tiling_dir, os.path.join(tiling_dir)[3]))
-    gdf_crs = rds.rio.crs
-    del rds
-
-    # Create a temp working directory for parallelization i/o
-    input_parallel_files = os.path.join(output_dir, 'shape_file_parallel_input')
-    os.makedirs(input_parallel_files, exist_ok=True)
-
-    # Make a unique directory in parallel directory
-    output_parallel_files = os.path.join(output_dir, 'shape_file_parallel_output')
-    os.makedirs(output_parallel_files, exist_ok=True)
-
-    logging.info('Starting shape file creation')
-
-    # Batch the input files for parallelization
-    tilling_dir_list = [os.path.join(tiling_dir, file) for file in os.listdir(tiling_dir)]
-    logging.info(f'Length of tilling dir: {len(os.listdir(tiling_dir))}')
-    batches = list(batch_list(tilling_dir_list, cores))
-
-    # Create the shape files in parallel
-    t1 = time.time()
-    for i, batch in enumerate(batches):
-        job_path = os.path.join(input_parallel_files, f'batch_{i}_input.json')
-        with open(job_path, 'w+') as f:
-            json.dump({'tiling_files': batch}, f)
-        outpath = os.path.join(output_parallel_files, f'batch_{i}_output.json')
-        Popen([sys.executable, os.path.join(BIN_DIR, 'make_shape_files.py'), '--input_file', job_path,
-               '--shape_dir', shape_dir, '--crs', gdf_crs.to_string(), '--outpath', outpath])
-
-    # Wait until all the job files have been written out
-    while len(os.listdir(shape_dir)) < len(tilling_dir_list) * 5:
-        print('SLEEPING')
-        time.sleep(3 * 60)
-
-    file_bounds = []
-    for file in os.listdir(output_parallel_files):
-        with open(os.path.join(output_parallel_files, file), 'r') as f:
-            f_data = json.load(f)
-            for bound in f_data['file_bounds']:
-                file_bounds.append((bound[0], Polygon(bound[1])))
-
-    logging.info(f'Made tiff and shape files in {time.time() - t1}s using {len(batches)} cores')
-    logging.info('Finished making tile tiff and shape files')
-
-    names, bounds = zip(*file_bounds)
-    geoseries = gpd.GeoSeries(data=bounds)
-    rdf = gpd.GeoDataFrame(data=names, columns=['name_shp'], geometry=geoseries, crs=gdf_crs)
-
-    shape_tiles = os.path.join(output_dir, 'shp_tiles_' + str(input_rstr.split('.')[0][-3:]))
-    rdf.to_file(shape_tiles)
-    logging.info('Finished making tile shp files')
-
-    if bucket_name is not None and s3 is not None:
-        for obj in os.listdir(shape_tiles):
-            s3.Bucket(bucket_name).upload_file(os.path.join(shape_tiles, obj), name + '_' + obj)
-
-    del rdf
-    logging.info('Finished uploading tile shp files to the S3 bucket')
-
-    return shape_tiles
+    return dss.crs
 
 
 def perform_inference(input_rstr: str, name: str, tiling_dir: str, output_dir: str, cores: int, model_path: str,
-                      shape_tiles: str, progress_file: str = None, bucket_name: str = None, s3=None):
+                      crs: rasterio.crs.CRS, progress_file: str = None, bucket_name: str = None, s3=None):
     logging.info('Starting Inference')
-    test_gdf = gpd.read_file(
-        os.path.join(output_dir, 'shp_tiles_' + str(input_rstr.split('.')[0][-3:]),
-                     'shp_tiles_' + str(input_rstr.split('.')[0][-3:]) + '.shp')
-    )
     np.random.seed(42)
 
     # Create input and output directories for parallelization i/o
@@ -278,14 +256,10 @@ def perform_inference(input_rstr: str, name: str, tiling_dir: str, output_dir: s
         job_path = os.path.join(input_parallel_files, f'batch_{i}_input.json')
         with open(job_path, 'w+') as f:
             json.dump({'tiling_files': batch}, f)
-        outpath = os.path.join(output_parallel_files, f'batch_{i}_output.json')
-        args = [sys.executable, os.path.join(BIN_DIR, 'inference.py'), '--input_file', job_path,
-                '--model_path', model_path,
-                '--shape_path', os.path.join(shape_tiles, 'shp_tiles_' + str(input_rstr.split('.')[0][-3:]) + '.shp'),
-                '--tiling_dir', tiling_dir, '--input_tiff', input_rstr, '--location_name', name,
-                '--outpath', outpath]
-
-        Popen(args)
+        out_path = os.path.join(output_parallel_files, f'batch_{i}_output.json')
+        Popen([sys.executable, os.path.join(BIN_DIR, 'inference.py'), '--input_file', job_path,
+               '--model_path', model_path, '--tiling_dir', tiling_dir, '--input_tiff', input_rstr, '--location_name',
+               name, '--crs', str(crs), '--outpath', out_path])
 
     logging.info(f"Performing inference on {count} files")
 
@@ -303,9 +277,11 @@ def perform_inference(input_rstr: str, name: str, tiling_dir: str, output_dir: s
         with open(os.path.join(output_parallel_files, file), 'r') as f:
             file_data = json.load(f)
             for geom in file_data['geoms']:
-                geometry = Polygon(geom[1])
+                geometry = Polygon(geom[1]['coordinates'][0])
                 geoms.append((geom[0], geometry, geom[2], geom[3], geom[4]))
 
+    # TODO: In the future progress files will just be the geojson that are output from the subprocesses. So turn any
+    #  existing CSV progress files into the same geojson file and then update the addition of its contents to geoms here
     if progress_file is not None:
         geoms += polygon_inference_file_to_list(progress_file, tiff_dir=tiling_dir)
 
@@ -315,7 +291,7 @@ def perform_inference(input_rstr: str, name: str, tiling_dir: str, output_dir: s
     geoseries_data = gpd.GeoSeries(data=geom_vals)
 
     test_gdff = gpd.GeoDataFrame(data=file_names, columns=['name_shp'], geometry=geoseries_data,
-                                 crs=test_gdf.crs)
+                                 crs=str(crs))
 
     test_gdff['label'] = pred_labels
     test_gdff['value'] = pred_values
@@ -328,13 +304,13 @@ def perform_inference(input_rstr: str, name: str, tiling_dir: str, output_dir: s
                                  str(input_rstr.split('.')[0][-3:]))
     test_gdff.to_file(inference_dir)
 
-    if bucket_name is not None:
+    if bucket_name is not None and s3 is not None:
         for obj in os.listdir(inference_dir):
             s3.Bucket(bucket_name).upload_file(os.path.join(inference_dir, obj), name + '_' + obj)
 
     logging.info('Finished uploading inference files to the S3 bucket')
 
-    del test_gdf, test_gdff, pred_labels, pred_values, fl_values
+    del test_gdff, pred_labels, pred_values, fl_values
 
 
 def do_inference(input_rstr: str, name: str, model_path: str, progress_file: str = None, cores: int = None,
@@ -357,12 +333,10 @@ def do_inference(input_rstr: str, name: str, model_path: str, progress_file: str
     s3 = boto3.resource('s3')
     bucket_name = s3_bucket_name
 
-    write_tiff_files(input_rstr=input_rstr, name=name, tiling_dir=tiling_dir, output_dir=output_dir,
-                     bucket_name=bucket_name, s3=s3)
-    shape_tiles = write_shape_files(input_rstr=input_rstr, name=name, tiling_dir=tiling_dir, shape_dir=shape_dir,
-                                    output_dir=output_dir, cores=cores, bucket_name=bucket_name, s3=s3)
+    crs = write_tiff_files(input_rstr=input_rstr, name=name, tiling_dir=tiling_dir, output_dir=output_dir,
+                           bucket_name=bucket_name, s3=s3)
     perform_inference(input_rstr=input_rstr, name=name, tiling_dir=tiling_dir, output_dir=output_dir, cores=cores,
-                      model_path=model_path, shape_tiles=shape_tiles, progress_file=progress_file,
+                      model_path=model_path, progress_file=progress_file, crs=crs,
                       bucket_name=bucket_name, s3=s3)
 
 
@@ -385,6 +359,5 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    do_inference(input_rstr=args.file, name=args.name, model_path=args.model_path, progress_file=args.progress_path,
+    do_inference(input_rstr=args.file, name=args.name, model_path=args.model, progress_file=args.progress_file,
                  cores=args.cores, output_dir=args.out_dir, s3_bucket_name=args.bucket_name)
-
--- tiling_and_inference/inference.py
+++ tiling_and_inference/inference.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 import argparse
-import geopandas as gpd
 from fastai.vision import *
-import boto3
 import time
+import geojson
 
 
-def main(input_file: str, model_path: str, shape_path: str, tiling_dir: str, input_tiff_path: str,
+def main(input_file: str, model_path: str, tiling_dir: str, input_tiff_path: str, tiff_geometries_path: str,
          location_name: str, output_file: str):
     job_id = os.path.basename(output_file).split('.json')[0]
     work_dir = os.path.dirname(output_file)
@@ -14,9 +13,6 @@ def main(input_file: str, model_path: str, shape_path: str, tiling_dir: str, inp
     t1 = time.time()
     # pkl file path
     learn_infer = load_learner(path=os.path.dirname(model_path), file=os.path.basename(model_path))
-
-    # shape file path
-    test_gdf = gpd.read_file(shape_path)
 
     tfms = get_transforms(flip_vert=True, max_lighting=0.1, max_zoom=1.05, max_warp=0.)
     np.random.seed(42)
@@ -34,72 +30,60 @@ def main(input_file: str, model_path: str, shape_path: str, tiling_dir: str, inp
         file_data = json.load(f)
     ls_names = file_data['tiling_files']
     print(f"{time.time() - t1}s setup time")
+
     # unique name for progress csv
-    csv_path = os.path.join(work_dir, f'{location_name}_{sent_indx}_Inference_Progress_{job_id}.csv')
     log_path = os.path.join(work_dir, f'{location_name}_{sent_indx}_Inference_Progress_{job_id}.log')
-    with open(csv_path, 'w+') as f:
-        f.write('Index,Filename,Geometry,Predicted_Label,Predicted_Value,fl_value\n')
     with open(log_path, 'w+') as f:
         f.write(f'Length of tiling files for job {len(ls_names)}\n')
-    tg = np.array([os.path.basename(i) for i in test_gdf['name_shp']])
+
+    # Read in tiff bounding boxes
+    with open(tiff_geometries_path, 'r') as f:
+        tiff_geometries = geojson.load(f)['geoms']
+
     # tiling file paths
-    for i, y in enumerate(ls_names):
+    for i, tiff_path in enumerate(ls_names):
         t0 = time.time()
-        diff1 = None
-        diff2 = None
+        diff = None
+
+        tiff_name = os.path.basename(tiff_path)
         try:
             t1 = time.time()
-            testt = np.where(tg == os.path.basename(y)[:-4] + '.shp')
-            diff1 = time.time() - t1
-            try:
-                if testt[0][0] != 0:
-                    t1 = time.time()
-                    im = test_data.train_ds[i][0]
-                    prediction = learn_infer.predict(im)
+            im = test_data.train_ds[i][0]
+            prediction = learn_infer.predict(im)
 
-                    pred_val = prediction[1].data.item()
-                    pred_label = val_dict[pred_val]
-                    fl_val = prediction[2].data[pred_val].item()
+            pred_val = prediction[1].data.item()
+            pred_label = val_dict[pred_val]
+            fl_val = prediction[2].data[pred_val].item()
 
-                    geom_bounds = list(list(test_gdf.iterrows())[testt[0][0]][1].geometry.boundary.coords)
-                    geoms.append((y, geom_bounds, pred_label, pred_val, fl_val))
-                    outline = f'{i},{y},{geom_bounds},{pred_label},{pred_val},{fl_val}'
-                    with open(csv_path, 'a') as f:
-                        f.write(outline + '\n')
-                    diff2 = time.time() - t1
-                else:
-                    outline = 'First index'
+            geom_bounds = tiff_geometries[tiff_name]
+            geoms.append((tiff_path, geom_bounds, pred_label, pred_val, fl_val))
+            with open(output_file, 'w+') as f:
+                geojson.dump({'geoms': geoms}, f, indent=1)
 
-                del im, prediction, pred_val, pred_label, fl_val, testt
+            outline = f'{i},{tiff_path},{geom_bounds},{pred_label},{pred_val},{fl_val}'
+            diff = time.time() - t1
 
-            except Exception as e:
-                outline = str(e)
-        except:
-            outline = 'failure'
+            del im, prediction, pred_val, pred_label, fl_val
+        except Exception as e:
+            outline = str(e)
 
-        if diff1 is not None:
-            outline += f" diff1 {diff1}s"
-        if diff2 is not None:
-            outline += f" diff2 {diff2}s"
-        outline += f' {time.time() - t0}s'
-
+        if diff is not None:
+            outline += f" inference time: {diff}s"
+        outline += f' Total time: {time.time() - t0}s'
         with open(log_path, 'a') as f:
             outline += '\n'
             f.write(outline)
-
-    with open(output_file, 'w+') as f:
-        json.dump({'geoms': geoms}, f, indent=1)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_file', type=str, required=True, help='Path to json file with tiling file paths')
     parser.add_argument('--model_path', type=str, required=True, help='Path to ML model')
-    parser.add_argument('--shape_path', type=str, required=True, help='Path to input shp file')
     parser.add_argument('--tiling_dir', type=str, required=True, help='Path to tiling directory')
     parser.add_argument('--input_tiff', type=str, required=True, help='Path to input tiff file')
+    parser.add_argument('--geom_path', type=str, required=True, help='Path to the tiff geometries file')
     parser.add_argument('--location_name', type=str, required=True, help='Location name of job')
     parser.add_argument('--outpath', type=str, required=True, help='Path to the output json file')
     args = parser.parse_args()
-    main(args.input_file, args.model_path, args.shape_path, args.tiling_dir, args.input_tiff, args.location_name,
+    main(args.input_file, args.model_path, args.tiling_dir, args.geom_path, args.input_tiff, args.location_name,
          args.outpath)
